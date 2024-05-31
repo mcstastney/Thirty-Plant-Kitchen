64461f7c2217cf89e98427f2528cedb26464b78f
Shafia <111538877+sfk30@users.noreply.github.com>
2024-05-31 21:50:09 +0100

    Imported Hero component into hompage

--- group-3-spring-24/src/pages/Home.js
+++ group-3-spring-24/src/pages/Home.js
@@ -1,29 +1,13 @@
 import { Link } from "react-router-dom";
 import Reviews from "../components/Reviews";
-import '../App.css';
+import Hero from "../components/Hero";
+import "../App.css";
 
 function Home() {
   return (
     <>
-      <h1>Welcome to Thirty Plant Kitchen!</h1>
-      <p>
-       Experts recommend that we eat 30 different plants a week.
-      </p>
-      <p>
-        We also know that eating local and seasonal food is healthier for us and the environment.
-      </p>
-      <p>
-        The Thirty Plant Kitchen app puts the ease in seasonal eating. 
-      </p>  
-      <p>  
-        Discover which plants are at their best and how to
-        incorporate more fruit and veg varieties in delicious, healthy recipes.
-      </p>
+      <Hero></Hero>
 
-      <Link to="/SignUp">
-        <button type="button">Sign up!</button>
-      </Link>
-      
       <Reviews></Reviews>
     </>
   );
