eac71105fc6fde60095bbb1d2b58d65b383b0226
Abbie Kennard-Jones <122180697+AbbieKJ@users.noreply.github.com>
2024-06-03 14:49:25 +0100

    signup styling

--- group-3-spring-24/src/pages/SignUp.js
+++ group-3-spring-24/src/pages/SignUp.js
@@ -6,6 +6,9 @@ import '../styles/SignUp.css'
 import { Typography } from '@mui/material';
 //img imports
 import SignupHero from '../assets/stock-imgs/signuphero.jpg';
+import Point1 from '../assets/stock-imgs/point1.jpg';
+import Point2 from '../assets/stock-imgs/point2.jpg';
+import Point3 from '../assets/stock-imgs/point3.jpg';
 
 function Home() {
     return (
@@ -18,16 +21,37 @@ function Home() {
           />
         <div className="signup-txt-container">
           <div className="signup-title-container">
-      <Typography variant="h1" >Join the club!</Typography>
-      <Typography variant="subtitle1" fontSize={'2.8rem'}>Register for free</Typography>
+      <Typography variant="h1" >Join the Club!</Typography>
+      <Typography variant="subtitle1" fontSize={'2.8rem'}>Get started for free</Typography>
       <div className="scroll-div">
       <ScrollButton/>
       </div>
       </div>
+      <div className="features-container-signup">
+        <div className="point1">
       <p>Become a Thirty Plant Kitchen member and curate your own personalised recipe library. </p>
+      <img
+      src={Point1}
+      alt="Tomatoes cooking in a pan"
+      />
+      </div>
+      <div className="point2">
+      <img
+      src={Point2}
+      alt="Holding a bunch of grapes at a picnic"
+      />
       <p>Plus, get exclusive access to our plant tracker and stay on top of your 30-a-week target!</p>
       </div>
-      <br></br>
+      <div className="point3">
+      <p>Plus, get exclusive access to our plant tracker and stay on top of your 30-a-week target!</p>
+      <img
+      src={Point3}
+      alt="Tomatoes cooking in a pan"
+      />
+      </div>
+      </div>
+      </div>
+      
       <div id="signup-form">
       <SignUpForm/>
       </div>
